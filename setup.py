d77acd7649ceeb1480f67b2c94bc5f7d4996529b
Connor Petri <114629025+connor-petri@users.noreply.github.com>
2022-10-05 16:30:00 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 import subprocess
 import sys
+import os
 
-def install(package):
+def brew_install(package):
+    os.system(f'brew install {package}')
+
+def pip_install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
 if __name__ == '__main__':
-    install('tk')
-    install('PySimpleGUI')
+    brew_install('pyenv')
+    brew_install('tcl-tk')
+    os.system('pyenv shell 3.10.6')
+    pip_install('PySimpleGUI')
