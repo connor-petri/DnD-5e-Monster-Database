d842cb8e3f166c5be48ff1cc4f3ab7d437763548
Connor Petri <114629025+connor-petri@users.noreply.github.com>
2022-10-05 17:24:19 -0700

    Add files via upload

--- resources/functions.py
+++ resources/functions.py
@@ -1,7 +1,26 @@
 import PySimpleGUI as sg
 import json
 
+import resources.monster as MonsterFile
+import resources.layout as gui
 
+
+# Setup for main function. Generates monster dictionary, loads save data, and initalizes main window from layout.py
+def set_up():
+
+    monster_dict = {}
+
+    for monster in MonsterFile.monster_generator(MonsterFile.monster_data):
+        monster_dict[monster.getName().lower()] = monster
+
+    with open("./resources/saves.json", "r", encoding="utf8") as save_file:
+            file_obj = json.load(save_file)
+            save_list = file_obj["save data"]
+
+    return monster_dict, gui.main(save_list), save_list
+
+
+# Dumps save data to saves.json
 def dump_save_data(path, data):
 
     with open(path, "w", encoding="utf8") as save_file:
@@ -9,6 +28,7 @@ def dump_save_data(path, data):
         save_file.write(json_obj)
 
 
+# Add save to save list
 def add_save(window, monster_dict, save_list, value):
 
     if (value in monster_dict.keys()) and (value not in save_list):
@@ -25,6 +45,7 @@ def add_save(window, monster_dict, save_list, value):
     return save_list
 
 
+# delete save from save list
 def delete_save(window, save_list, value):
 
     if value in save_list:
