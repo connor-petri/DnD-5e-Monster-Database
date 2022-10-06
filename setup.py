61b1f99be808383acabaab56ba42788087e1c00a
Connor Petri <114629025+connor-petri@users.noreply.github.com>
2022-10-05 18:45:48 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,40 +1,12 @@
 import subprocess
 import sys
-import os
-import platform
 
-
-def brew_install(package):
-    os.system(f'brew install {package}')
 
 def pip_install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-
-def mac_install():
-
-    brew_install('pyenv')
-    brew_install('tcl-tk')
-
-    pip_install('PySimpleGUI')
-
-
-
-def windows_install():
-
-    pip_install('PySimpleGui')
-
-
-def main():
-
-    if platform.system() == "Windows":
-        windows_install()
-
-    if platform.system() in ('Darwin', 'macOS'):
-        mac_install()
     
     
 
 if __name__ == '__main__':
     
-    main()
+    pip_install('PySimpleGUI')
